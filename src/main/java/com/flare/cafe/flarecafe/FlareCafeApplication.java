b4050a121e072a84595f66faa33df61d4fb82718
daisy-sseob <orolsyeo@gmail.com>
2025-04-13 16:09:42 +0900

    settigns

---
+++ src/main/java/com/flare/cafe/flarecafe/FlareCafeApplication.java
@@ -0,0 +1,13 @@
+package com.flare.cafe.flarecafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlareCafeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FlareCafeApplication.class, args);
+  }
+
+}

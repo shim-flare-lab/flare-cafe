5d3d6cbbfb75f9afd3b2d5e840c3b140b6f0ac3d
bright-flare <orolsyeo@gmail.com>
2025-05-07 23:26:32 +0900

    feat: add money class

---
+++ src/main/java/com/flarecafe/feature/generic/Money.java
@@ -0,0 +1,82 @@
+package com.flarecafe.feature.generic;
+
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Getter
+public class Money {
+  
+  public static final Money ZERO = Money.won(0);
+
+  private final BigDecimal amount;
+
+  Money(BigDecimal amount) {
+    this.amount = amount;
+  }
+
+  public static Money won(long amount) {
+    return new Money(BigDecimal.valueOf(amount));
+  }
+
+  public static Money won(double amount) {
+    return new Money(BigDecimal.valueOf(amount));
+  }
+
+  public Money plus(Money amount) {
+    return new Money(this.amount.add(amount.amount));
+  }
+
+  public Money minus(Money amount) {
+    return new Money(this.amount.subtract(amount.amount));
+  }
+
+  public Money times(double percent) {
+    return new Money(this.amount.multiply(BigDecimal.valueOf(percent)));
+  }
+
+  public Money divide(double divisor) {
+    return new Money(amount.divide(BigDecimal.valueOf(divisor)));
+  }
+
+  public boolean isLessThan(Money other) {
+    return amount.compareTo(other.amount) < 0;
+  }
+
+  public boolean isGreaterThanOrEqual(Money other) {
+    return amount.compareTo(other.amount) >= 0;
+  }
+
+  public Long longValue() {
+    return amount.longValue();
+  }
+
+  public Double doubleValue() {
+    return amount.doubleValue();
+  }
+
+  public String toString() {
+    return amount.toString() + "원";
+  }
+  
+
+  @Override
+  public boolean equals(Object other) {
+    if (this == other) {
+      return true;
+    }
+
+    if (other == null || getClass() != other.getClass()) {
+      return false;
+    }
+
+    Money money = (Money) other;
+    return Objects.equals(amount.doubleValue(), money.amount.doubleValue());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(amount.doubleValue());
+  }
+}
